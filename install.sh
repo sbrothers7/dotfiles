940bef571cb07bf17bbf635ff860c396c0700cbe
sbrothers7 <92313509+sbrothers7@users.noreply.github.com>
2025-07-01 16:13:22 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,40 +7,71 @@ BOLD_RED="\033[1;31m"
 RESET="\033[0m"
 
 # Install xCode CLI tools
-echo "${BOLD_BLUE}Installing commandline tools...${RESET}"
-xcode-select --install
+echo "${BOLD_BLUE}Installing command line tools...${RESET}"
+if xcode-select -p &>/dev/null; then
+  echo "${BOLD_GREEN}xCode CLI is already installed. Continuing...${RESET}"
+else
+  xcode-select --install
+fi
 
-echo "${BOLD_BLUE}Checking Homebrew Installation...${RESET}"
+echo "${BOLD_BLUE}\nChecking Homebrew Installation...${RESET}"
 if ! command -v brew >/dev/null 2>&1; then
   echo "${BOLD_RED}Homebrew is not installed. Please install Homebrew first: https://brew.sh/${RESET}"
   exit 1
+else
+  echo "${BOLD_GREEN}Found homebrew installation. Continuing..."
 fi
 
 ## Taps
-echo "${BOLD_BLUE}Tapping Brew...${RESET}"
+echo "${BOLD_BLUE}\nTapping Brew...${RESET}"
 brew tap FelixKratz/formulae
 brew tap koekeishiya/formulae
 
 ## Formulae
 echo "${BOLD_BLUE}Installing Brew Formulae...${RESET}"
-brew install yabai borders skhd < /dev/null
-brew install gsl boost libomp wget jq ripgrep bear mas gh < /dev/null
-read -n1 -rep "${BOLD_BLUE}Install sketchybar? [y/n]: ${RESET}" choice
-if [[ choice =~ ^[Yy]$ ]]; then
-  brew install sketchybar ifstat switchaudio-osx lua < /dev/null # sketchybar & dependencies for config
+
+echo -n "${BOLD_BLUE}\nInstall tiling window manager? [y/n]: ${RESET}"
+read -k 1 yabai
+echo
+if [[ $yabai =~ ^[Yy]$ ]]; then
+  brew install yabai skhd < /dev/null
+else
+  echo "${BOLD_GREEN}Skipping...${RESET}"
 fi
+
+echo -n "${BOLD_BLUE}\nInstall borders? [y/n]: ${RESET}"
+read -k 1 borders
+echo
+if [[ $yabai =~ ^[Yy]$ ]]; then
+  brew install borders < /dev/null
+else
+  echo "${BOLD_GREEN}Skipping...${RESET}"
+fi
+
+echo -n "${BOLD_BLUE}\nInstall sketchybar? [y/n]: ${RESET}" 
+read -k 1 sketchybar
+echo
+if [[ $sketchybar =~ ^[Yy]$ ]]; then
+  brew install sketchybar ifstat switchaudio-osx lua < /dev/null # sketchybar & dependencies for config
+else
+  echo "${BOLD_GREEN}Skipping...${RESET}"
+fi
+
 brew install lf ctpv btop neovim < /dev/null # utilities
+brew install gsl boost libomp wget jq ripgrep bear mas gh < /dev/null
 brew install git lazygit stow < /dev/null
 brew install starship fzf zsh-syntax-highlighting zsh-autosuggestions autojump < /dev/null # zsh
 brew install ffmpeg mono node python armadillo < /dev/null # big packages
 
 ## Casks
-echo "${BOLD_BLUE}Installing Brew Casks...${RESET}"
+echo "${BOLD_BLUE}\nInstalling Brew Casks...${RESET}"
 brew install --cask kitty \
 brave-browser \
 karabiner-elements
-read -n1 -rep "${BOLD_BLUE}Install other utilities? [y/n]: ${RESET}" choice
-if [[ choice =~ ^[Yy]$ ]]; then
+echo -n "${BOLD_BLUE}\nInstall other utility casks? [y/n]: ${RESET}"
+read -k 1 utility 
+echo
+if [[ $utility =~ ^[Yy]$ ]]; then
   brew install --cask
   zoom \
   sol \
@@ -48,16 +79,18 @@ if [[ choice =~ ^[Yy]$ ]]; then
   command-x \
   yellowdot \
   middleclick \
+  linearmouse \
   mpv
 else
   echo "${BOLD_GREEN}Skipping..."
 fi
 
 ### Fonts
-brew install --cask sf-symbols font-sf-mono font-sf-pro font-hack-nerd-font font-jetbrains-mono font-fira-code
 
-read -n1 -rep "${BOLD_BLUE}Automatically change macOS defaults? [y/n]: ${RESET}" choice
-if [[ choice =~ ^[Yy]$ ]]; then
+echo -n "${BOLD_BLUE}\nAutomatically change macOS defaults? [y/n]: ${RESET}"
+read -k 1 choice
+echo
+if [[ $choice =~ ^[Yy]$ ]]; then
   # macOS Settings
   echo "${BOLD_BLUE}Changing macOS defaults...${RESET}"
   defaults write com.apple.NetworkBrowser BrowseAllInterfaces 1
@@ -87,50 +120,70 @@ if [[ choice =~ ^[Yy]$ ]]; then
   defaults write NSGlobalDomain WebKitDeveloperExtras -bool true
   defaults write com.apple.mail AddressesIncludeNameOnPasteboard -bool false
   defaults write -g NSWindowShouldDragOnGesture -bool true
+  echo "${BOLD_GREEN}Successfully changed macOS defaults.${RESET}"
 else
   echo "${BOLD_GREEN}Skipping...${RESET}"
 fi
 
-# Copying configuration files
-echo "${BOLD_BLUE}Planting Configuration Files...${RESET}"
-[ ! -d "$HOME/dotfiles" ] && git clone "https://github.com/sbrothers7/dotfiles" $HOME/dotfiles
-
 # Installing Fonts
-echo "${BOLD_BLUE}Installing fonts...${RESET}"
-git clone "https://github.com/shaunsingh/SFMono-Nerd-Font-Ligaturized.git" /tmp/SFMono_Nerd_Font
-mv /tmp/SFMono_Nerd_Font/* $HOME/Library/Fonts
-rm -rf /tmp/SFMono_Nerd_Font/
-
-curl -L "https://github.com/kvndrsslr/sketchybar-app-font/releases/download/v2.0.28/sketchybar-app-font.ttf" -o $HOME/Library/Fonts/sketchybar-app-font.ttf
+echo -n "${BOLD_BLUE}\nInstall fonts and symbols? [y/n]: ${RESET}"
+read -k 1 fonts
+echo 
+if [[ $fonts =~ ^[Yy]$ ]]; then
+  echo "${BOLD_BLUE}\nInstalling fonts...${RESET}"
+  brew install --cask sf-symbols font-sf-mono font-sf-pro font-hack-nerd-font font-jetbrains-mono font-fira-code
+  git clone "https://github.com/shaunsingh/SFMono-Nerd-Font-Ligaturized.git" /tmp/SFMono_Nerd_Font
+  mv /tmp/SFMono_Nerd_Font/* $HOME/Library/Fonts
+  rm -rf /tmp/SFMono_Nerd_Font/
+else
+  echo "${BOLD_GREEN}Skipping...${RESET}"
+fi
 
 # SBar Lua
-(git clone https://github.com/FelixKratz/SbarLua.git /tmp/SbarLua && cd /tmp/SbarLua/ && make install && rm -rf /tmp/SbarLua/)
+if [[ $sketchybar =~ ^[Yy]$ ]]; then
+  curl -L "https://github.com/kvndrsslr/sketchybar-app-font/releases/download/v2.0.28/sketchybar-app-font.ttf" -o $HOME/Library/Fonts/sketchybar-app-font.ttf
 
-source $HOME/dotfiles/.zshrc
-cfg config --local status.showUntrackedFiles no
+  (git clone https://github.com/FelixKratz/SbarLua.git /tmp/SbarLua && cd /tmp/SbarLua/ && make install && rm -rf /tmp/SbarLua/)
+fi
+
+# Copying configuration files
+echo -n "${BOLD_BLUE}\nUse config files? [y/n]: ${RESET}"
+read -k 1 config
+echo
+if [[ $config =~ ^[Yy]$ ]]; then
+  echo "${BOLD_BLUE}Planting Configuration Files...${RESET}"
+  [ ! -d "$HOME/dotfiles" ] && git clone "https://github.com/sbrothers7/dotfiles" $HOME/dotfiles
+  source $HOME/dotfiles/.zshrc
+  cfg config --local status.showUntrackedFiles no
+  
+  echo "${BOLD_BLUE}Attempting to stow items to root folder...${RESET}"
+  rm -rf .zprofile
+  cd dotfiles
+  stow .
+  echo "${BOLD_BLUE}If operation fails, remove all dotfile in root then try manually.${RESET}"
+fi
 
 echo "${BOLD_GREEN}All dependencies installed successfully.${RESET}"
 
-echo "${BOLD_BLUE}Attempting to stow items to root folder...${RESET}"
-rm -rf .zprofile
-cd dotfiles
-stow .
-echo "${BOLD_BLUE}If operation fails, remove all dotfile in root then try manually.${RESET}"
-
 # Start Services
-echo "${BOLD_BLUE}Starting Services (grant permissions)...${RESET}"
+echo "${BOLD_BLUE}\nStarting Services (grant permissions)...${RESET}"
 yabai --start-service
 skhd --start-service
 brew services start sketchybar
 brew services start borders
 
+echo "${BOLD_BLUE}\nChecking SIP status...${RESET}"
 csrutil status
-echo "(optional) Disable SIP for advanced yabai features."
-echo ""
+echo "${BOLD_BLUE}Partly disable SIP for advanced yabai features.\nThis can be achieved by going into recovery mode, then entering:"
+echo "${RESET}csrutil enable --without fs --without debug --without nvram"
+echo "${BOLD_BLUE}into the terminal.\n"
 
-read -n1 -rep "${BOLD_BLUE}Add sudoer for yabai features? [y/n]: ${RESET}" choice
-if [[ $choice =~ ^[Yy]$ ]]; then
+echo -n "${BOLD_BLUE}Add sudoer for yabai features? [y/n]: ${RESET}"
+read -k 1 yabaisudoer 
+echo
+if [[ $yabaisudoer =~ ^[Yy]$ ]]; then
   $(whoami) ALL = (root) NOPASSWD: sha256:$(shasum -a 256 $(which yabai) | awk "{print \$1;}") $(which yabai) --load-sa' to '/private/etc/sudoers.d/yabai
-else
-  echo "${BOLD_GREEN}Installation complete. Enter${RESET} source .zshrc ${BOLD_GREEN} to apply changes to zsh.${RESET}"
 fi
+
+echo "${BOLD_GREEN}Installation complete."
+source .zshrc
