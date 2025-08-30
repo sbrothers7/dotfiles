4a6de78eac609eeabe7339f5222dd3c80a65ffd1
sbrothers7 <92313509+sbrothers7@users.noreply.github.com>
2025-08-30 13:16:33 +0900

    minor changes

--- .config/nvim/lua/vim-options.lua
+++ .config/nvim/lua/vim-options.lua
@@ -29,9 +29,9 @@ end)
 
 vim.opt.breakindent = true -- Enable break indent
 
-vim.o.tabstop = 4
-vim.o.softtabstop = 4
-vim.o.shiftwidth = 4
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
 
 vim.opt.undofile = true -- Save undo history
 
